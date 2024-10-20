0f812884f4458ce30e74b432f7624749212bea72
Arthur Milin <arturmilin94@gmail.com>
2024-10-20 19:26:46 +0300

    task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null
+        this.max = null
+        this.answer = null
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        return this.answer = Math.round((this.min + this.max) / 2) 
     }
 
     lower() {
-
+        this.max = this.answer
     }
 
     greater() {
-
+        this.min = this.answer
     }
 }
 
 module.exports = GuessingGame;
+
+
